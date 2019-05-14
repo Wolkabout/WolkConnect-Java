efe1ca1d531b04e6a554081038104bed151b1440
mzlicic <milan.zlicic@wolkabout.com>
2019-05-14 11:20:08 +0200

    Added simple example

---
+++ src/main/java/examples/simple/Example.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2019 WolkAbout Technology s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package examples.simple;
+
+import com.wolkabout.wolk.Wolk;
+import com.wolkabout.wolk.protocol.ProtocolType;
+
+import java.util.concurrent.TimeUnit;
+
+public class Example {
+    public static void main(String[] args) {
+
+        final Wolk wolk = Wolk.builder()
+                .mqtt()
+                .host("ssl://api-demo.wolkabout.com:8883")
+                .sslCertification("ca.crt")
+                .deviceKey("device_key")
+                .password("some_password")
+                .build()
+                .protocol(ProtocolType.JSON_SINGLE_REFERENCE)
+                .build();
+
+        wolk.connect();
+
+        while (true) {
+            try {
+                double randomTemp = Math.random() * 100 - 20; // random number between -20 and 80
+
+                wolk.addReading("T", Double.toString(randomTemp));
+
+                wolk.publish();
+
+                TimeUnit.MILLISECONDS.sleep(200);
+            } catch (Exception e) {}
+        }
+    }
+}
